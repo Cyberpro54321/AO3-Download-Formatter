cfd78bbcb54217ff4e762c56e9046367f7c4359e
Cyberpro123 <rebycorp321@gmail.com>
2025-03-02 23:37:34 -0800

    can now find the Raws folder

--- process.py
+++ process.py
@@ -3,6 +3,7 @@
 import argparse  # https://docs.python.org/3/library/argparse.html
 import configparser  # https://docs.python.org/3/library/configparser.html
 import csv  # https://docs.python.org/3/library/csv.html
+import os.path  # https://docs.python.org/3/library/os.path.html
 
 parser = argparse.ArgumentParser()
 parser.add_argument("rawName")
@@ -12,6 +13,13 @@ args = parser.parse_args()
 config = configparser.ConfigParser()
 config.read(args.config)
 
+rawName = args.rawName
+if rawName[-5:] != ".html":
+    rawName = rawName + ".html"
+dirStorageRaw = config["General"]["dir_storage"]
+dirStorageProcessed = os.path.abspath(os.path.expanduser(dirStorageRaw))
+if dirStorageProcessed[-1:] != "/":
+    dirStorageProcessed = dirStorageProcessed + "/"
+dirRaws = config["General"]["dir_raws"]
 
-print(args.rawName)
-print(config["G"]["t"])
+print("Searching for " + rawName + " in " + dirStorageProcessed + dirRaws)
