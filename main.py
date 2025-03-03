0098297724ac7eda5f53bcdef8dd0ae1fe450aaf
Cyberpro123 <rebycorp321@gmail.com>
2025-03-02 23:20:54 -0800

    extremely basic configparser implimentation

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import csv  # https://docs.python.org/3/library/csv.html
 
 parser = argparse.ArgumentParser()
 parser.add_argument("rawName")
+parser.add_argument("-c", "--config", default="config/config.ini")
 args = parser.parse_args()
 
+config = configparser.ConfigParser()
+config.read(args.config)
+
+
 print(args.rawName)
+print(config["G"]["t"])
