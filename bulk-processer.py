b6b9e06c3f3ad6fcd688f3818194cc3a6a05b164
Cyberpro123 <rebycorp321@gmail.com>
2025-03-04 19:41:40 -0800

    now calls process.py with the --quiet option instead of --silent, added --quiet option to prevent listing of works

--- bulk-processer.py
+++ bulk-processer.py
@@ -10,6 +10,7 @@ import argparse  # https://docs.python.org/3/library/argparse.html
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--config", default="config/config.ini")
 parser.add_argument("-d", "--database", default="config/db.csv")
+parser.add_argument("-q", "--quiet", action="store_true")
 args = parser.parse_args()
 if not os.path.exists(args.config):
     if args.config == "config/config.ini":
@@ -29,5 +30,6 @@ dirRawsFull = dirStorage + config["dir"]["raws"]
 
 allRaws = os.listdir(dirRawsFull)
 for work in allRaws:
-    print("Now formatting: " + work)
-    subprocess.run(["./process.py", "-c", args.config, "-d", args.database, "-s", work])
+    if not args.quiet:
+        print("Now formatting: " + work)
+    subprocess.run(["./process.py", "-c", args.config, "-d", args.database, "-q", work])
